a7bece56cfbf742e26982a3ef35faffb422df5dc
sanxincao <1449779840@qq.com>
2020-12-14 00:12:28 +0800

    ligon api

--- api.py
+++ api.py
@@ -1,10 +1,23 @@
 from flask import Flask
-from flask_restful import Api,Resource
+#import parser
+from flask_restful import Api,Resource,reqparse,abort
+import time
 #this file write api route
 app = Flask(__name__)
 api = Api(app)
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('task', type=str)
+class root(Resource):
+    def get():
+        return {
+            'token':'13840702430'
+        }
+
 #the server node
 """private int id;
+        http://localhost/
         private string name;
         private string icon;s
         private string remark;
@@ -22,16 +35,38 @@ class severnode(Resource):
         'remark':'null',
         'linkcount':'1',
         'selected':True,
-        'host':
-        'port:8080'
+        'host':'144.34.148.204',
+        'method':'aes-156-gcm',
+        'port':'443',
+        'password':'password'
         }
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(severnode, '/servernode')
+#USER_LOGIN_API login data
+"""
+
+"""
+class userlogin(Resource):
+    def post(self):
+        args = parser.parse_args()
+        now=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        print(args)
+        return {
+            'avatar':'null',
+            'serviceexpiredate':now,
+            'phone':'13840702430',
+            'token':'13840702430',
+            'type':'null',
+            'password':'null',
+            'message_id':'null',
+            'message_content':'null'
+        }
+api.add_resource(userlogin, '/login')
 
 class DES_KEY(Resource):
     def get(self):
         return [ 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF ]
 
-class
+
 if __name__ == '__main__':
     app.run(debug=True)
