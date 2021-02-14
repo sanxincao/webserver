4db63167eb5f59e3ca75b5448239a52693892fa2
sanxincao <1449779840@qq.com>
2021-02-15 01:58:28 +0800

    zanshi

---
+++ app/auth/views.py
@@ -0,0 +1,46 @@
+from flask import render_template, redirect, request, url_for, flash
+from . import auth
+from flask_login import login_user, logout_user, login_required, \
+    current_user
+from .. import db
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid email or password.')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data.lower(),
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/secrect')
+@login_required
+def secrect():
+  return 'need login'
--- manager.py
+++ manager.py
@@ -1,20 +1,20 @@
 import os
 import click
-from flask_migrate import Migrate
+from flask_migrate import Migrate,MigrateCommand
 from app import create_app, db
 from flask_script import Manager,Shell
 from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app,db)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
-
+manager.add_command('db',MigrateCommand)
 @app.cli.command()
 @click.argument('test_names', nargs=-1)
 def test(test_names):
@@ -27,7 +27,7 @@ def test(test_names):
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__=='__main__':
-  app.run()
+  manager.run()
 
 
 
