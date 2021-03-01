41433224a8a25580275afcae19bdeb2906792199
sanxincao <1449779840@qq.com>
2021-03-01 23:10:15 +0800

    finish servernode api

--- app/api_1_0/userlogin.py
+++ app/api_1_0/userlogin.py
@@ -2,6 +2,8 @@ from flask_restful import Api,Resource,reqparse,abort
 from flask_httpauth import HTTPBasicAuth,HTTPTokenAuth
 from ..models import User,AnonymousUser
 from flask import g, jsonify,request
+from flask_login import login_user, logout_user, login_required, \
+    current_user
 import time
 from . import api_bp
 
@@ -45,9 +47,11 @@ class userlogin(Resource):
       phone=new_x['phone']
       password=new_x['password']
       islogin=verify_password(phone,password)
+      print(islogin)
       now=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+      if islogin == True:
 
-      return {
+        return {
           'code':'200',
           'content':
           {
@@ -59,7 +63,68 @@ class userlogin(Resource):
               'type':'1',
               'token':g.current_user.generate_auth_token(expiration=3600)
           }
+        }
+      else :
+        return {
+          'code':'400'
+        }
 
-      }
 
 api.add_resource(userlogin, '/login')
+
+
+
+# todo servernode logout api get sources from datebase and update
+
+#the server node
+"""private int id;
+        http://localhost/
+        private string name;
+        private string icon;s
+        private string remark;
+        private int linkcount;
+        private bool selected;
+        private string host;
+        private string port;
+        private string method;
+        private string password;"""
+class severnode(Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('token')
+        args = parser.parse_args()
+        print(args)
+
+        return {
+        'code':'200',
+        'content':[
+        {
+        'id': '1',
+        'name':'test',
+        'icon':'white.loge',
+        'remark':'null',
+        'linkcount':'1',
+        'selected':True,
+        'host':'144.34.148.204',
+        'port':'443',
+        'method':'aes-256-gcm',
+        'password':'password'
+        },
+        {
+        'id': '2',
+        'name':'test',
+        'icon':'white.loge',
+        'remark':'null',
+        'linkcount':'1',
+        'selected':True,
+        'host':'144.34.148.204',
+        'port':'443',
+        'method':'aes-256-gcm',
+        'password':'password'
+        }
+
+        ]
+        }
+
+api.add_resource(severnode,'/servernode')
