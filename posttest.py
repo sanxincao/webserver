5e379ecec2417084bd678a8cdbf67d11253afb84
sanxincao <1449779840@qq.com>
2021-03-16 00:17:25 +0800

    do commmit

--- posttest.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-url="http://127.0.0.1:5000/api/v1.0/login"
-data={
-    'phone': 'Ec9C/XMDbtAnQrOMF51g4w==',
-    'password': 'FZz3LAvSqBjRKk7RbJ2cAQ=='
-}
-requests.post(url,json=data)
