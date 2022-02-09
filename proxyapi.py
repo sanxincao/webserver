3eb87baf10cd5bdf8d4a4c58148f98b05e2547f9
sanxincao <1449779840@qq.com>
2022-02-09 23:54:29 +0800

    proxy api

--- proxyapi.py
+++ proxyapi.py
@@ -1,6 +1,7 @@
 from flask import Flask,request
 #import parser
 from flask_restful import Api,Resource,reqparse,abort
+import requests
 import time
 import binascii
 import base64
@@ -38,89 +39,8 @@ des=DES()
 parser = reqparse.RequestParser()
 
 
-#the server node
-"""private int id;
-        http://localhost/
-        private string name;
-        private string icon;s
-        private string remark;
-        private int linkcount;
-        private bool selected;
-        private string host;
-        private string port;
-        private string method;
-        private string password;"""
-class severnode(Resource):
 
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('token')
-        args = parser.parse_args()
-        print(args)
-        return {
-        'code':'200',
-        'content':[
-        {
-        'id': '1',
-        'name':'test',
-        'icon':'white.loge',
-        'remark':'null',
-        'linkcount':'1',
-        'selected':True,
-        'host':'144.34.148.204',
-        'port':'443',
-        'method':'aes-256-gcm',
-        'password':'password'
-        },
-        {
-        'id': '2',
-        'name':'test',
-        'icon':'white.loge',
-        'remark':'null',
-        'linkcount':'1',
-        'selected':True,
-        'host':'144.34.148.204',
-        'port':'443',
-        'method':'aes-256-gcm',
-        'password':'password'
-        }
 
-        ]
-        }
-
-#api.add_resource(severnode, '/')
-api.add_resource(severnode,'/servernode')
-#USER_LOGIN_API login data
-"""
-
-"""
-class userlogin(Resource):
-  def get(self):
-    return {'test':'test'}
-
-  def post(self):
-      new_x = request.get_json()
-      phone=new_x['phone']
-      password=new_x['password']
-      print(new_x)
-
-      now=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-
-      return {
-          'code':'200',
-          'content':
-          {
-              'avatar':'null',
-              'serviceexpiredate':now,
-
-              'password':'null',
-              'phone':'null',
-              'type':'1',
-              'token':'123456'
-          }
-
-      }
-api.add_resource(userlogin, '/login')
 
 class DES_KEY(Resource):
     def get(self):
